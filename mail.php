a6b5e49da86f5154e429bd75acaf8659eb2a36be
Pisich <62033118+Pisich@users.noreply.github.com>
2020-08-15 13:14:37 -0500

    Update mail.php
    Functional mail forms

--- mail.php
+++ mail.php
@@ -2,7 +2,7 @@
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
-$recipient = "emailaddress@here.com";
+$recipient = "croc1110@hotmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
